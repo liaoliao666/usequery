b4d5df63e0f1846e316e341fe6e7bea88a7427f8
liaoliao666 <1076988944@qq.com>
2021-01-14 13:17:55 +0800

    chore

--- examples/simple/vite.config.ts
+++ examples/simple/vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    hmr: process.env.DEV_REMOTE && {
+    hmr: {
       protocol: 'wss',
     },
   },
