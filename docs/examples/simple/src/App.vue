49cfba2e83d79e148870a2c19a876421f1c9bbb8
liaoliao666 <1076988944@qq.com>
2021-01-14 09:42:40 +0800

    chore

---
+++ docs/examples/simple/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { toRefs } from 'vue'
+import { useQuery } from 'v-use-query'
+
+const result = useQuery('repoData', () =>
+  fetch('https://api.github.com/repos/liaoliao666/v-use-query').then(res =>
+    res.json()
+  )
+)
+const { isLoading, error, data, isFetching } = toRefs(result)
+</script>
+
+<template>
+  <span v-if="isLoading">Loading...</span>
+  <span v-else-if="error">Loading...</span>
+  <div v-else>
+    <h1>{{ data.name }}</h1>
+    <p>{{ data.description }}</p>
+    <strong>👀 {{ data.subscribers_count }}</strong>
+    <strong>✨ {{ data.stargazers_count }}</strong>
+    <strong>🍴 {{ data.forks_count }}</strong>
+    <div>{{ isFetching ? 'Updating...' : '' }}</div>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ docs/examples/simple/src/AppWithClient.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { QueryClientProvider, QueryClient } from 'v-use-query'
+
+import App from './App.vue'
+
+const queryClient = new QueryClient()
+</script>
+
+<template>
+  <QueryClientProvider :client="queryClient">
+    <App />
+  </QueryClientProvider>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
