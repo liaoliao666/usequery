86fce22c159ca518aa45947080341aa7c5aadfe4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-14 01:51:51 +0000

    Deploy liaoliao666/v-use-query to github.com/liaoliao666/v-use-query.git:gh-pages

--- assets/reference_hydration_hydrate.md.ffe66aec.lean.js
+++
@@ -1,1 +0,0 @@
-import{l as e,f as t,G as n}from"./framework.dc3bd9a4.js";const o='{"title":"hydration/hydrate","description":"","frontmatter":{"id":"hydration/hydrate","title":"hydration/hydrate"},"relativePath":"reference/hydration/hydrate.md","lastUpdated":1610472514943}',a={},i=n('',4);a.render=function(n,o,a,s,d,r){return e(),t("div",null,[i])};export default a;export{o as __pageData};
--- assets/reference_useMutation.md.3d3153d5.lean.js
+++
@@ -1,1 +0,0 @@
-import{l as e,f as t,G as n}from"./framework.dc3bd9a4.js";const o='{"title":"useMutation","description":"","frontmatter":{"id":"useMutation","title":"useMutation"},"relativePath":"reference/useMutation.md","lastUpdated":1610472514947}',i={},a=n('',5);i.render=function(n,o,i,s,l,r){return e(),t("div",null,[a])};export default i;export{o as __pageData};
