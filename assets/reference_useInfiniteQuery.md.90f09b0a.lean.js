02dc37f94ff9b854e614a3a40608c41aa6e958ab
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-17 17:23:28 +0000

    Deploy liaoliao666/vu-query to github.com/liaoliao666/vu-query.git:gh-pages

--- assets/reference_useInfiniteQuery.md.90f09b0a.lean.js
+++
@@ -1,1 +0,0 @@
-import{l as e,f as a,G as n}from"./framework.dc3bd9a4.js";const t='{"title":"useInfiniteQuery","description":"","frontmatter":{"id":"useInfiniteQuery","title":"useInfiniteQuery"},"relativePath":"reference/useInfiniteQuery.md","lastUpdated":1610801842881}',s={},o=n('',7);s.render=function(n,t,s,i,l,u){return e(),a("div",null,[o])};export default s;export{t as __pageData};
