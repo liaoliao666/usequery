75f60741c97fea19ddfcabd0d0420cfcdbec36ef
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-25 02:26:11 +0000

    Deploy liaoliao666/vu-query to github.com/liaoliao666/vu-query.git:gh-pages

---
+++ assets/guides_important-defaults.md.95d1e23e.lean.js
@@ -0,0 +1,1 @@
+import{l as e,f as t,G as o}from"./framework.dc3bd9a4.js";const a='{"title":"Important Defaults","description":"","frontmatter":{"id":"important-defaults","title":"Important Defaults"},"relativePath":"guides/important-defaults.md","lastUpdated":1611541543572}',n={},i=o("",11);n.render=function(o,a,n,r,u,s){return e(),t("div",null,[i])};export default n;export{a as __pageData};
